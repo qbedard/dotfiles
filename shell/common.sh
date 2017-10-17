f9803aef76b1edeee1f0fffa4c567bddc97e7a8c
Tim Bedard <tpbedard@gmail.com>
2017-10-17 14:29:21 -0500

    initial *shrc config

---
+++ shell/common.sh
@@ -0,0 +1,33 @@
+# ----- PATH ----- #
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:~/.composer/vendor/bin:$PATH
+
+# ----- Aliases ----- #
+
+# --- General ---
+alias l="ls -1"
+alias q="exit"
+alias quit="exit"
+alias vz="vim ~/.zshrc"
+alias zr="source ~/.zshrc"
+alias vr="vim ~/.vim/vimrc"
+alias vb="vim ~/.vim/vimrc.bundles"
+
+# --- Git ---
+alias gs="git status"
+alias ga="git add"
+alias gaa="git add ."
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gr="git reset"
+alias gb="git branch"
+alias gbc="git checkout -b"
+alias gco="git checkout"
+alias gd="git diff"
+alias gm="git merge"
+alias gp="git push"
+alias gl="git pull"
+
+# ----- Misc ----- #
+# 10ms for key sequences
+KEYTIMEOUT=1
+
