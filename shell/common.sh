0e2949a235d75c5ff23c7de57087aeadb508a889
Tim Bedard <tpbedard@gmail.com>
2018-02-18 00:44:01 -0600

    disabled UT aliases, added hub alias

--- shell/common.sh
+++ shell/common.sh
@@ -35,23 +35,23 @@ alias gp="git push"
 alias gl="git pull"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 
-# ----- PyPE  ----- #
-alias vu="vagrant up"
-alias vh="vagrant halt"
-alias vss="vagrant ssh"
-alias pw="pype_workon"
-alias pw2.8="pype_workon 27.8.0"
-alias pw2.11="pype_workon 27.11.0"
-alias pw3.11="pype_workon 36.11.0"
-alias dj="python manage.py"
-alias djrs="python manage.py runserver"
-alias djrss="python manage.py runsslserver"
-alias djsh="python manage.py shell"
-alias djsu="python manage.py createsuperuser"
-alias djm="python manage.py migrate"
-alias djmm="python manage.py makemigrations"
-alias vs="vim settings.py"
-alias vl="vim local_settings.py"
+# ----- UT PyPE  ----- #
+# alias vu="vagrant up"
+# alias vh="vagrant halt"
+# alias vss="vagrant ssh"
+# alias pw="pype_workon"
+# alias pw2.8="pype_workon 27.8.0"
+# alias pw2.11="pype_workon 27.11.0"
+# alias pw3.11="pype_workon 36.11.0"
+# alias dj="python manage.py"
+# alias djrs="python manage.py runserver"
+# alias djrss="python manage.py runsslserver"
+# alias djsh="python manage.py shell"
+# alias djsu="python manage.py createsuperuser"
+# alias djm="python manage.py migrate"
+# alias djmm="python manage.py makemigrations"
+# alias vs="vim settings.py"
+# alias vl="vim local_settings.py"
 
 # ----- Misc ----- #
 
@@ -60,6 +60,9 @@ export EDITOR=nvim
 
 # 10ms for key sequences
 KEYTIMEOUT=1
+
+# git -> hub
+eval "$(hub alias -s)"
 
 # fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
