c86b30b3c779c2b497924b7d966ca764d529055f
Tim Bedard <tpbedard@gmail.com>
2018-03-11 23:59:04 -0500

    homebrew.sh -> install.sh

--- scripts/mac/bootstrap.sh
+++ scripts/mac/bootstrap.sh
@@ -1,78 +1,9 @@
 #!/bin/bash
 
-# Note: This script requires that the following be done before running:
-# 1. Install Xcode.
-# 2. Install Command Line Tools.
-# 3. Accept the CLT agreement.
+DOTFILES="$HOME/.dotfiles"
 
-# Things this script does not install:
-# - Magnet
-
-# Homebrew
-if test ! "$(which brew)"; then
-    echo "Installing Homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-# Update recipes
-echo "Updating Homebrew..."
-brew update
-
-echo "Installing packages..."
-PACKAGES=(
-    autoconf
-    awscli
-    ctags
-    direnv
-    fasd
-    fzf
-    git
-    hub
-    neovim
-    node
-    npm
-    python@2
-    python3
-    ranger
-    ruby
-    shellcheck
-    the_silver_searcher
-    tree
-    zsh
-    zsh-completions
-)
-brew install "${PACKAGES[@]}"
-
-echo "Cleaning up..."
-brew cleanup
-
-echo "Installing Cask..."
-brew tap caskroom/cask
-brew tap caskroom/versions
-
-echo "Installing Cask apps..."
-CASKS=(
-    alfred
-    firefox-developer-edition
-    insomnia
-    iterm2
-    sequel-pro
-    slack
-    virtualbox
-)
-brew cask install "${CASKS[@]}"
-
-echo "Installing fonts..."
-brew tap caskroom/fonts
-FONTS=(
-    font-source-code-pro-for-powerline
-)
-brew cask install "${FONTS[@]}"
-
-echo "Installing Oh My Zsh..."
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
-echo "Installing fzf..."
-/usr/local/opt/fzf/install
-
-echo "Installion complete."
+. "$DOTFILES/scripts/mac/config.sh"
+. "$DOTFILES/scripts/mac/install.sh"
+. "$DOTFILES/scripts/python.sh"
+. "$DOTFILES/scripts/ruby.sh"
+. "$DOTFILES/scripts/mac/link.sh"
