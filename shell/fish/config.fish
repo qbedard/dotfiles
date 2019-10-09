ce29f28e127ba441b08d553b2612a98b04000f65
Tim Bedard <tpbedard@gmail.com>
2019-10-09 12:55:02 -0500

    theme, colors

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,19 +1,13 @@
-# Fisher
-# if not functions -q fisher
-#     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-#     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-#     fish -c fisher
-# end
-
 # Oh My Fish
 # if not functions -q omf
 #     curl -L https://get.oh-my.fish | fish
 # end
 
-# theme_gruvbox dark
-
 # Modify the terminal's 256 color palette to use the gruvbox theme
-# set -l GRUVBOX_SCRIPT "$HOME/.local/share/nvim/site/plugged/gruvbox/gruvbox_256palette.sh"
-# if test -f $GRUVBOX_SCRIPT
-#   bash $GRUVBOX_SCRIPT
-# end
+set -l GRUVBOX_SCRIPT "$HOME/.local/share/nvim/site/plugged/gruvbox/gruvbox_256palette.sh"
+if test -f $GRUVBOX_SCRIPT
+  bash $GRUVBOX_SCRIPT
+end
+
+set -g theme_nerd_fonts yes
+set theme_color_scheme gruvbox
