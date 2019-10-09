ebb3c2485c74bc24d68600b8b4e60b23ac796124
Tim Bedard <tpbedard@gmail.com>
2019-10-09 12:48:44 -0500

    bobthefish

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,11 +1,14 @@
 # Fisher
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
+# if not functions -q fisher
+#     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+#     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+#     fish -c fisher
+# end
 
-eval (starship init fish)
+# Oh My Fish
+# if not functions -q omf
+#     curl -L https://get.oh-my.fish | fish
+# end
 
 # theme_gruvbox dark
 
