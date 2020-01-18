1ec9f2a6ffdf17ec5ab67ef881bb593e8a320892
Tim Bedard <tpbedard@gmail.com>
2020-01-18 00:54:55 -0600

    add fish colortest function

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -135,8 +135,19 @@ export PYTHONDONTWRITEBYTECODE=1  # prevent .pyc files
 # --- direnv ---
 direnv hook fish | source
 
-if status is-interactive
-and not set -q TMUX
-    # attach to session "main" if it exists, otherwise create it
-    exec tmux new-session -A -s main
+# color test
+function colortest
+    awk 'BEGIN{
+        s="/\\\\/\\\\/\\\\/\\\\/\\\\"; s=s s s s s s s s;
+        for (colnum = 0; colnum<77; colnum++) {
+            r = 255-(colnum*255/76);
+            g = (colnum*510/76);
+            b = (colnum*255/76);
+            if (g>255) g = 510-g;
+            printf "\033[48;2;%d;%d;%dm", r,g,b;
+            printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+            printf "%s\033[0m", substr(s,colnum+1,1);
+        }
+        printf "\n";
+    }'
 end
