7822aefd5ce3f810f342a29cafe711576310fd73
Tim Bedard <tpbedard@gmail.com>
2020-07-22 22:21:46 -0500

    fzf ripgrep func tweaks

--- config/fish/functions/__fzf_ripgrep.fish
+++ config/fish/functions/__fzf_ripgrep.fish
@@ -1,29 +1,3 @@
-# function fzf_ripgrep
-
-#   set -l commandline (__fzf_parse_commandline)
-#   set -l fzf_query $commandline[2]
-
-#   rg  |
-#   fzf --height=50% --reverse --multi --preview-window right:70% \
-#     --preview 'git show --color=always {} | head -200' \
-#     --query $fzf_query |
-#   while read -l s; set results $results $s; end
-
-#   if test -z "$results"
-#     commandline -f repaint
-#     return
-#   else
-#     commandline -t ""
-#   end
-
-#   for result in $results
-#     commandline -it -- (string escape $result)
-#     commandline -it -- " "
-#   end
-#   commandline -f repaint
-
-# end
-
 function __fzf_ripgrep
   set -l rg_cmd "rg -l --hidden --no-ignore-vcs --glob '!.git/*' --glob '!.direnv/*'"
 
@@ -31,17 +5,14 @@ function __fzf_ripgrep
     FZF_DEFAULT_COMMAND="rg --files" fzf \
       -m \
       -e \
-      --ansi \
       --phony \
       --reverse \
       --bind "ctrl-a:select-all" \
-      --bind "f12:execute-silent:(subl -b {})" \
       --bind "change:reload:$rg_cmd {q} || true" \
       --preview "rg -i --pretty --context 2 {q} {}" | cut -d":" -f1,2
   )
 
   if test -n "$results"
-    # $EDITOR $results
     for result in $results
       commandline -it -- (string escape $result)
       commandline -it -- " "
