c725e5a7003178b4c297586144f75743a5993abf
Tim Bedard <tpbedard@gmail.com>
2020-07-25 00:42:51 -0500

    fzf git branch tweak

--- config/fish/functions/__fzf_git_branch.fish
+++ config/fish/functions/__fzf_git_branch.fish
@@ -5,11 +5,18 @@ function __fzf_git_branch
   set -l commandline (__fzf_parse_commandline)
   set -l fzf_query $commandline[2]
 
-  git branch --all --color=always --sort committerdate | grep -v '/HEAD\s' --color=always |
-  sed 's/^..//' | cut -d' ' -f1 |
-  sed 's#remotes/##' |
+  git branch --all --color=never --sort committerdate |
+    grep -v '/HEAD\s' |
+    sed 's/^..//' |
+    sed -E 's/remotes\/.*\///g' |
+    # sed -E 's/remotes\///g' |
+    uniq |
   fzf --height=50% --reverse --ansi --multi --tac --preview-window right:70% \
-    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" {} | head -200' \
+    --preview '__glog_preview {}' \
+    --preview '''
+      git log --oneline --graph --date=short --color=always \
+        --pretty="format:%C(auto)%cd %h%d %s" {} -- | head -200
+      ''' \
     --query $fzf_query |
   while read -l s; set results $results $s; end
 
