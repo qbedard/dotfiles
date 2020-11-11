8c481f53d3cff25a182f1141d7c342615cd59b78
Tim Bedard <tpbedard@gmail.com>
2020-11-11 16:59:12 -0600

    more gruvbox treesitter tweaks

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -172,6 +172,10 @@ require('packer').startup(function()
   }
   -- use 'nvim-treesitter/nvim-treesitter-refactor'
   -- use { 'nvim-treesitter/completion-treesitter', opt = true }
+  use {
+    'nvim-treesitter/playground',
+    requires = 'nvim-treesitter/nvim-treesitter',
+  }
 
   -- TODO: Switch?
   -- use {
@@ -200,37 +204,68 @@ require('packer').startup(function()
       vim.cmd('highlight! link FoldColumn LineNr')
 
       -- treesitter highlights
-      vim.cmd('highlight! link TSPunctDelimiter GruvboxFg3')
-      vim.cmd('highlight! link TSPunctBracket GruvboxOrange')
-      vim.cmd('highlight! link TSPunctSpecial Special')
-      vim.cmd('highlight! link TSConstant Constant')
-      vim.cmd('highlight! link TSConstBuiltin GruvboxOrange')
-      vim.cmd('highlight! link TSConstMacro Constant')
-      vim.cmd('highlight! link TSString String')
-      vim.cmd('highlight! link TSStringRegex String')
-      vim.cmd('highlight! link TSStringEscape GruvboxAqua')
-      vim.cmd('highlight! link TSCharacter Character')
-      vim.cmd('highlight! link TSNumber Number')
-      vim.cmd('highlight! link TSBoolean Boolean')
-      vim.cmd('highlight! link TSFloat Float')
-      vim.cmd('highlight! link TSFunction GruvboxAqua')  -- not Function
-      vim.cmd('highlight! link TSFuncBuiltin GruvboxOrange')
-      vim.cmd('highlight! link TSFuncMacro Macro')
-      vim.cmd('highlight! link TSParameter GruvboxFg')
-      vim.cmd('highlight! link TSMethod GruvboxGreen')
-      vim.cmd('highlight! link TSField GruvboxGreen')
-      vim.cmd('highlight! link TSProperty GruvboxFg1')
-      vim.cmd('highlight! link TSConstructor GruvboxOrange')
-      vim.cmd('highlight! link TSConditional Conditional')
-      vim.cmd('highlight! link TSRepeat Repeat')
-      vim.cmd('highlight! link TSLabel Label')
-      vim.cmd('highlight! link TSOperator Operator')
-      vim.cmd('highlight! link TSKeyword Keyword')
-      vim.cmd('highlight! link TSException Exception')
-      vim.cmd('highlight! link TSType Type')
-      vim.cmd('highlight! link TSTypeBuiltin GruvboxOrange')
-      vim.cmd('highlight! link TSStructure Structure')
-      vim.cmd('highlight! link TSInclude GruvboxRed')  -- not Include
+      -- vim.cmd('highlight! link TSPunctDelimiter GruvboxFg3')  -- '.', ','
+      vim.cmd('highlight! link TSConstBuiltin Constant')  -- None
+      vim.cmd('highlight! link TSKeywordOperator Keyword')  -- not, in
+      vim.cmd('highlight! link TSVariableBuiltin Constant')  -- self
+      vim.cmd('highlight! link TSOperator GruvboxRed')
+      vim.cmd('highlight! link TSFunction GruvboxAqua')
+      vim.cmd('highlight! link TSMethod GruvboxAqua')
+
+      ----
+
+      -- vim.cmd('highlight! link TSPunctDelimiter Delimiter')  -- , . etc
+      -- vim.cmd('highlight! link TSPunctBracket Delimiter')
+      -- vim.cmd('highlight! link TSPunctSpecial Delimiter')
+      -- vim.cmd('highlight! link TSConstant Constant')
+      -- vim.cmd('highlight! link TSConstBuiltin Special')  -- None, etc
+      -- vim.cmd('highlight! link TSConstMacro Define')
+      -- vim.cmd('highlight! link TSString String')
+      -- vim.cmd('highlight! link TSStringRegex String')
+      -- vim.cmd('highlight! link TSStringEscape SpecialChar')
+      -- vim.cmd('highlight! link TSCharacter Character')
+      -- vim.cmd('highlight! link TSNumber Number')
+      -- vim.cmd('highlight! link TSBoolean Boolean')
+      -- vim.cmd('highlight! link TSFloat Float')
+
+      -- vim.cmd('highlight! link TSFunction Function')
+      -- vim.cmd('highlight! link TSFuncBuiltin Special')  -- print()
+      -- vim.cmd('highlight! link TSFuncMacro Macro')
+      -- vim.cmd('highlight! link TSParameter Identifier')
+      -- vim.cmd('highlight! link TSParameterReference TSParameter')
+      -- vim.cmd('highlight! link TSMethod Function')
+      -- vim.cmd('highlight! link TSField Identifier')
+      -- vim.cmd('highlight! link TSProperty Identifier')
+      -- vim.cmd('highlight! link TSConstructor Special')  -- __init__()
+      -- vim.cmd('highlight! link TSAnnotation PreProc')
+      -- vim.cmd('highlight! link TSAttribute PreProc')
+      -- vim.cmd('highlight! link TSNamespace Include')
+
+      -- vim.cmd('highlight! link TSConditional Conditional')  -- if
+      -- vim.cmd('highlight! link TSRepeat Repeat')  -- for, while
+      -- vim.cmd('highlight! link TSLabel Label')
+      -- vim.cmd('highlight! link TSOperator Operator')
+      -- vim.cmd('highlight! link TSKeyword Keyword')
+      -- vim.cmd('highlight! link TSKeywordFunction Keyword')
+      -- vim.cmd('highlight! link TSKeywordOperator Operator')  -- not, in
+      -- vim.cmd('highlight! link TSException Exception')
+
+      -- vim.cmd('highlight! link TSType Type')
+      -- vim.cmd('highlight! link TSTypeBuiltin Type')
+      -- vim.cmd('highlight! link TSInclude Include')
+
+      -- vim.cmd('highlight! link TSVariableBuiltin Special')  -- self
+
+      -- vim.cmd('highlight! link TSText TSNone')
+      -- vim.cmd('highlight! TSStrong term=bold cterm=bold gui=bold')
+      -- vim.cmd('highlight! TSEmphasis term=italic cterm=italic gui=italic')
+      -- vim.cmd('highlight! TSUnderline term=underline cterm=underline gui=underline')
+      -- vim.cmd('highlight! link TSTitle Title')
+      -- vim.cmd('highlight! link TSLiteral String')
+      -- vim.cmd('highlight! link TSURI Underlined')
+
+      -- vim.cmd('highlight! link TSTag Label')
+      -- vim.cmd('highlight! link TSTagDelimiter Delimiter')
     end,
   }
   use {
@@ -364,6 +399,7 @@ require('packer').startup(function()
       vim.g.javascript_plugin_jsdoc = 1
       vim.g.vim_markdown_new_list_item_indent = 2
     end,
+    disable = true,
   }
 
 end)
