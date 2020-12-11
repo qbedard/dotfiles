c30f8d0f3f2f2153cdf7ab1440b11bef5c748f76
Tim Bedard <tpbedard@gmail.com>
2020-12-11 10:53:49 -0600

    lsp map tweaks

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -165,12 +165,12 @@ require("packer").startup {
             nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>
             " nnoremap <silent> gd <cmd>lua vim.lsp.buf.declaration()<CR>
             " nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR>
-            " nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>
-            nnoremap <silent> gD <cmd>lua vim.lsp.buf.implementation()<CR>
-            " nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
+            nnoremap <silent> <leader>k <cmd>lua vim.lsp.buf.hover()<CR>
+            " nnoremap <silent> gD <cmd>lua vim.lsp.buf.implementation()<CR>
+            " nnoremap <silent> <leader>K <cmd>lua vim.lsp.buf.signature_help()<CR>
             nnoremap <silent> 1gD <cmd>lua vim.lsp.buf.type_definition()<CR>
             nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>
-            " nnoremap <silent> g0 <cmd>lua vim.lsp.buf.document_symbol()<CR>
+            nnoremap <silent> g0 <cmd>lua vim.lsp.buf.document_symbol()<CR>
             nnoremap <silent> gf <cmd>lua vim.lsp.buf.formatting()<CR>
           ]],
           false
