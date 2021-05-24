d972d41aa8170362592855042c1f262d0f0eb538
Tim Bedard <tpbedard@gmail.com>
2021-05-23 22:15:11 -0500

    add ruby 3 gems to PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,6 +19,7 @@ fish_add_path "$HOME/.cargo/bin"  # rust
 fish_add_path "$HOME/go/bin"  # go
 fish_add_path "/usr/local/opt/ruby/bin"  # ruby
 fish_add_path "/usr/local/lib/ruby/gems/2.7.0/bin"  # ruby gems
+fish_add_path "/usr/local/lib/ruby/gems/3.0.0/bin"  # ruby gems
 
 # add openssl for compilers
 set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
