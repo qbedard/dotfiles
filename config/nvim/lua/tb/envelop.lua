0ab0f5cbe2156b55bf5319a558743d15ea600ad4
Tim Bedard <tpbedard@gmail.com>
2021-06-28 11:27:31 -0500

    update vscode language servers

--- config/nvim/lua/tb/envelop.lua
+++ config/nvim/lua/tb/envelop.lua
@@ -3,29 +3,29 @@ vim.g.envelop_node_link = {
   "node_modules/.bin/luafmt",
   "node_modules/.bin/prettier",
   "node_modules/.bin/bash-language-server",
-  "node_modules/.bin/css-languageserver",
   "node_modules/.bin/docker-langserver",
-  "node_modules/.bin/html-languageserver",
+  "node_modules/.bin/neovim-node-host",
   "node_modules/.bin/pyright",
   "node_modules/.bin/pyright-langserver",
   "node_modules/.bin/typescript-language-server",
   "node_modules/.bin/vim-language-server",
-  "node_modules/.bin/vscode-json-languageserver",
+  "node_modules/.bin/vscode-css-language-server",
+  "node_modules/.bin/vscode-html-language-server",
+  "node_modules/.bin/vscode-json-language-server",
   "node_modules/.bin/yaml-language-server"
 }
 vim.g.envelop_node_packages = {
   "bash-language-server",
   "dockerfile-language-server-nodejs",
   "lua-fmt",
+  "neovim",
   "prettier",
   -- "prettier-plugin-toml",
   "pyright",
   "typescript",
   "typescript-language-server",
   "vim-language-server",
-  "vscode-css-languageserver-bin",
-  "vscode-html-languageserver-bin",
-  "vscode-json-languageserver",
+  "vscode-langservers-extracted",
   "yaml-language-server"
 }
 vim.g.envelop_python3_link = {
