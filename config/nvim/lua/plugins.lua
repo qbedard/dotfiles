3e8634f56e65b1b7047cc8abf8ae968d55671fb2
Tim Bedard <tpbedard@gmail.com>
2021-12-01 00:40:37 -0600

    try out null-ls over efm-langserver

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -254,21 +254,66 @@ return require("packer").startup({
     })
 
     use({
-      "glepnir/lspsaga.nvim",
-      after = { "gruvbox.nvim", "gruvqueen" },
+      "jose-elias-alvarez/null-ls.nvim",
+      requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
       config = function()
-        require("tb.lspsaga")
+        local null_ls = require("null-ls")
+        local code_actions = null_ls.builtins.code_actions
+        local diagnostics = null_ls.builtins.diagnostics
+        local formatting = null_ls.builtins.formatting
+        -- TODO: fish lint
 
-        local map = require("tb.utils").map
-        map("n", "gh", ":Lspsaga lsp_finder<CR>")
-        map("n", "<leader>ca", ":Lspsaga code_action<CR>")
-        -- map("n", "<leader>k", ":Lspsaga hover_doc<CR>")
-        map("n", "gs", ":Lspsaga signature_help<CR>")
-        map("n", "gr", ":Lspsaga rename<CR>")
-        map("n", "gD", ":Lspsaga preview_definition<CR>")
-        map("n", "<leader>cd", ":Lspsaga show_line_diagnostics<CR>")
-        map("n", "[d", ":Lspsaga diagnostic_jump_prev<CR>")
-        map("n", "]d", ":Lspsaga diagnostic_jump_next<CR>")
+        local h = require("null-ls.helpers")
+        local flake8 = {
+          method = null_ls.methods.DIAGNOSTICS,
+          filetypes = { "python" },
+          generator = null_ls.generator({
+            command = "flake8",
+            args = { "--format", "default", "--stdin-display-name", "$FILENAME", "-" },
+            to_stdin = true,
+            from_stderr = true,
+            format = "line",
+            check_exit_code = function(code)
+              return code == 0 or code == 255
+            end,
+            on_output = h.diagnostics.from_pattern(
+              [[:(%d+):(%d+): ((%u)%w+) (.*)]],
+              { "row", "col", "code", "severity", "message" },
+              {
+                severities = {
+                  E = h.diagnostics.severities["error"],
+                  B = h.diagnostics.severities["warning"],
+                  C = h.diagnostics.severities["warning"],
+                  W = h.diagnostics.severities["warning"],
+                  D = h.diagnostics.severities["information"],
+                  F = h.diagnostics.severities["information"],
+                  N = h.diagnostics.severities["information"],
+                },
+              }
+            ),
+          }),
+          factory = h.generator_factory,
+        }
+
+        null_ls.config({
+          sources = {
+            code_actions.gitsigns,
+            -- diagnostics.flake8,
+            flake8,
+            diagnostics.rubocop,
+            diagnostics.shellcheck,
+            formatting.black,
+            formatting.fish_indent,
+            formatting.isort,
+            formatting.prettier,
+            formatting.rubocop,
+            formatting.shfmt,
+            formatting.sqlformat,
+            formatting.stylua,
+            formatting.terraform_fmt,
+          },
+        })
+        require("lspconfig")["null-ls"].setup({})
       end,
     })
 
