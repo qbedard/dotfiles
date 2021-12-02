0ceb6502a9f4952f12ad963e42788bd7f0e2a773
Tim Bedard <tpbedard@gmail.com>
2021-12-02 14:39:00 -0600

    add pyclean fish function

---
+++ config/fish/functions/pyclean.fish
@@ -0,0 +1,6 @@
+function pyclean
+    find . -name "*.py[co]" -type f -delete
+    find . -name __pycache__ -type d -delete
+    rm -rf **/*.egg-info .eggs .pytest_cache .tox \
+        build dist pip-wheel-metadata report.xml
+end
