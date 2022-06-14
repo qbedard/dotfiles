4d241c48dbbe363e4ce75b66b4d26f20a631676b
Tim Bedard <tpbedard@gmail.com>
2022-06-14 09:57:28 -0500

    flake8 has plugins in master now

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -247,9 +247,7 @@ return require("packer").startup({
     })
 
     use({
-      -- "jose-elias-alvarez/null-ls.nvim",
-      "timbedard/null-ls.nvim",
-      branch = "add-common-flake8-plugin-severities",
+      "jose-elias-alvarez/null-ls.nvim",
       requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
       config = function()
         require("tb.null-ls")
--- config/nvim/lua/tb/null-ls.lua
+++ config/nvim/lua/tb/null-ls.lua
@@ -3,52 +3,11 @@ local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
--- local h = require("null-ls.helpers")
-
--- local diagnostics_flake8 = {
---   name = "flake8",
---   method = null_ls.methods.DIAGNOSTICS,
---   filetypes = { "python" },
---   generator = null_ls.generator({
---     command = "flake8",
---     args = {
---       "--format=default",
---       "--select=C,E,F,W,B,N,B950",
---       "--ignore=E203,E501,N818,W503",
---       "--stdin-display-name=$FILENAME",
---       "-",
---     },
---     to_stdin = true,
---     from_stderr = true,
---     format = "line",
---     check_exit_code = function(code)
---       return code == 0 or code == 255
---     end,
---     on_output = h.diagnostics.from_pattern(
---       [[:(%d+):(%d+): ((%u)%w+) (.*)]],
---       { "row", "col", "code", "severity", "message" },
---       {
---         severities = {
---           E = h.diagnostics.severities["error"],
---           B = h.diagnostics.severities["warning"],
---           C = h.diagnostics.severities["warning"],
---           W = h.diagnostics.severities["warning"],
---           D = h.diagnostics.severities["information"],
---           F = h.diagnostics.severities["information"],
---           N = h.diagnostics.severities["information"],
---         },
---       }
---     ),
---   }),
---   factory = h.generator_factory,
--- }
-
 require("null-ls").setup({
   debug = true,
   sources = {
     code_actions.gitsigns,
     diagnostics.fish,
-    -- diagnostics.flake8,
     diagnostics.flake8,
     -- diagnostics.mypy,
     -- diagnostics.rubocop,
