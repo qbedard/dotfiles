f3011b3ed7a1ecc988e0f7fc6391804827ee3c49
Tim Bedard <tpbedard@gmail.com>
2022-10-15 11:42:21 -0500

    cmp_nvim_lsp.update_capabilities -> ...default_capabilities

--- config/nvim/lua/tb/lsp.lua
+++ config/nvim/lua/tb/lsp.lua
@@ -33,7 +33,7 @@ vim.fn.sign_define("DiagnosticSignHint", {
   texthl = "DiagnosticSignHint",
 })
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(
+local capabilities = require("cmp_nvim_lsp").default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
 capabilities.textDocument.completion.snippetSupport = false
