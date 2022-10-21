b5d70b1bcafa021562fca612d3e1999a4acf0da0
Tim Bedard <tpbedard@gmail.com>
2022-10-21 00:56:06 -0500

    toggleterm curved

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -402,7 +402,11 @@ return require("packer").startup({
     use({
       "akinsho/toggleterm.nvim",
       config = function()
-        require("toggleterm").setup({ open_mapping = "<c-t>", direction = "float" })
+        require("toggleterm").setup({
+          open_mapping = "<c-t>",
+          direction = "float",
+          float_opts = { border = "curved" },
+        })
       end,
     })
 
