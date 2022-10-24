f1b56e8018dad0c05446ee351adb1c3605bf1558
Tim Bedard <tpbedard@gmail.com>
2022-10-24 16:49:18 -0500

    cmp shenanigans

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -282,38 +282,80 @@ return require("packer").startup({
     use({
       "hrsh7th/nvim-cmp",
       requires = {
+        -- "hrsh7th/cmp-buffer",
+        -- "hrsh7th/cmp-cmdline",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-nvim-lua",
+        -- "hrsh7th/cmp-path",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
       },
-      after = "lspkind.nvim",
+      after = { "lspkind.nvim" },
+      -- after = { "lspkind.nvim", "nvim-web-devicons" },
       config = function()
         require("cmp_nvim_lsp").setup()
         local cmp = require("cmp")
         local lspkind = require("lspkind")
         cmp.setup({
+          -- window = {
+          --   completion = cmp.config.window.bordered(),
+          --   documentation = cmp.config.window.bordered(),
+          -- },
           formatting = { format = lspkind.cmp_format({ mode = "symbol" }) },
+          -- NOTE: Only useful for cmp-path
+          -- formatting = {
+          --   format = function(entry, vim_item)
+          --     if vim.tbl_contains({ "path" }, entry.source.name) then
+          --       local icon, hl_group = require("nvim-web-devicons").get_icon(
+          --         entry:get_completion_item().label
+          --       )
+          --       if icon then
+          --         vim_item.kind = icon
+          --         vim_item.kind_hl_group = hl_group
+          --         return vim_item
+          --       end
+          --     end
+          --     return lspkind.cmp_format({ mode = "symbol" })(entry, vim_item)
+          --   end,
+          -- },
+
           min_length = 0, -- allow for `from package import _` in Python
-          mapping = {
+          mapping = cmp.mapping.preset.insert({
             ["<S-Tab>"] = cmp.mapping.select_prev_item(),
             ["<Tab>"] = cmp.mapping.select_next_item(),
-            ["<C-p>"] = cmp.mapping.select_prev_item(),
-            ["<C-n>"] = cmp.mapping.select_next_item(),
             ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.close(),
             ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
-          },
-          sources = {
-            { name = "nvim_lsp" },
-            { name = "nvim_lua" },
-          },
+          }),
+          sources = { { name = "nvim_lsp" }, { name = "nvim_lua" } },
           snippet = {
             expand = function(args)
               require("luasnip").lsp_expand(args.body)
             end,
           },
         })
+
+        -- TODO: Get this working?
+        -- cmp.setup.cmdline({ "/", "?" }, {
+        --   -- completion = { autocomplete = false },
+        --   mapping = cmp.mapping.preset.cmdline({
+        --     ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        --     ["<Tab>"] = cmp.mapping.select_next_item(),
+        --     ["<C-Space>"] = cmp.mapping.complete(),
+        --     ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
+        --   }),
+        --   sources = { { name = "buffer" } },
+        -- })
+        --
+        -- cmp.setup.cmdline(":", {
+        --   -- completion = { autocomplete = false },
+        --   mapping = cmp.mapping.preset.cmdline({
+        --     ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        --     ["<Tab>"] = cmp.mapping.select_next_item(),
+        --     ["<C-Space>"] = cmp.mapping.complete(),
+        --     ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
+        --   }),
+        --   sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+        -- })
       end,
     })
 
