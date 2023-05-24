8786dca2bd4ebb2fbad7b64f80d2f1cb697dcf32
Tim Bedard <tpbedard@gmail.com>
2023-05-24 16:31:15 -0500

    comment out TS playground for now

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
+    -- "nvim-treesitter/playground",
     "windwp/nvim-ts-autotag",
   },
   opts = {
