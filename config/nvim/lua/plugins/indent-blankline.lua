8d8657a59e694c27782476b9f8615ebca591edc5
Tim Bedard <tpbedard@gmail.com>
2023-09-29 09:18:00 -0500

    update indent-blankline config

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -1,14 +1,15 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   opts = {
-    char = require("icons").bar.thin,
-    char_highlight_list = { "GruvboxBg1", "GruvboxBg2" },
-    -- context_highlight_list = { "GruvboxAqua" },
-    buftype_exclude = { "help", "nofile", "prompt", "quickfix", "terminal" },
-    filetype_exclude = { "text", "markdown" },
-    -- show_current_context = true,
-    show_first_indent_level = false,
-    show_trailing_blankline_indent = false,
+    exclude = {
+      filetypes = { "text", "markdown" },
+      buftypes = { "help", "nofile", "prompt", "quickfix", "terminal" },
+    },
+    indent = {
+      char = require("icons").bar.thin,
+      highlight = { "GruvboxBg1", "GruvboxBg2" },
+    },
+    whitespace = { remove_blankline_trail = true },
   },
-  -- enabled = false,
 }
