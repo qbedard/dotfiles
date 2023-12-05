146474a510f4099e68aa7da1e31c2fdb897778c5
Tim Bedard <tpbedard@gmail.com>
2023-12-05 13:26:42 -0600

    disable lspsnip

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -7,8 +7,8 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lua",
     -- "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
+    -- "L3MON4D3/LuaSnip",
+    -- "saadparwaiz1/cmp_luasnip",
   },
   opts = function()
     require("cmp_nvim_lsp").setup()
@@ -45,11 +45,11 @@ return {
         ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
       }),
       sources = { { name = "nvim_lsp" }, { name = "nvim_lua" } },
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
+      -- snippet = {
+      --   expand = function(args)
+      --     require("luasnip").lsp_expand(args.body)
+      --   end,
+      -- },
     }
 
     -- TODO: Get this working?
